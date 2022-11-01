dcb8c372b4438e652e7e0b481cb0d1dbb3c905c5
Joe Klein SEI <josephklein87@gmail.com>
2022-11-01 16:06:57 -0400

    changed some sizing on home page

--- Website/app.js
+++ Website/app.js
@@ -3,18 +3,19 @@ $(() => {
 // HOME PAGE VARIABLES AND LOAD CONDITIONS
 let testimonialVisible = false
 
+$('#testimonials-container').css("display", "flex")
 $('#slide-div').hide();
-$('#expand-less1').hide()
+$('#expand-more1').hide()
 
 //SLIDE FUNCTION TO REVEAL TESTIMONIALS
 const testimonialsReveal = () => {
     if (testimonialVisible === false){
-        $('#expand-more1').hide()
-        $('#expand-less1').show()
+        $('#expand-less1').hide()
+        $('#expand-more1').show()
         testimonialVisible = true
     } else if (testimonialVisible === true) {
-        $('#expand-more1').show()
-        $('#expand-less1').hide()
+        $('#expand-less1').show()
+        $('#expand-more1').hide()
         testimonialVisible = false
     }
     $('#slide-div').slideToggle("slow")
